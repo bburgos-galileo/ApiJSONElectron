29a735844ab34d6c91de717ca6d90d32e1d2dfaa
Byron Burgos <bburgos@PLUSTI.COM>
2022-03-28 22:08:21 -0600

    revisión

--- render.js
+++ render.js
@@ -1,6 +1,7 @@
 let tipo = document.getElementById('cmbTypeImage');
 let filtro = document.getElementById('cmbTypeCat');
 let imagen = document.getElementById('imgResult');
+let respuesta = document.getElementById('txtRespuesta');
 
 let boton = document.getElementById('btnBuscar');
 
@@ -14,15 +15,22 @@ function clickBoton() {
             .then(response => response.json())
             .then(dato => {
                 console.log(dato);
-                imagen.setAttribute('src', `https://cataas.com/${dato.url}`)
-            })
+                imagen.setAttribute('src', `https://cataas.com/${dato.url}`);
+                respuesta.value = "id: " + dato.id + "\nurl: " + dato.url + "\ncreado el: " + dato.created_at;
+            });
     } else if (iTipo != '' && iFiltro == '') {
         fetch(`https://cataas.com/cat?type=${iTipo}&json=true`)
             .then(response => response.json())
-            .then(dato => { imagen.setAttribute('src', `https://cataas.com/${dato.url}`) })
+            .then(dato => {
+                imagen.setAttribute('src', `https://cataas.com/${dato.url}`);
+                respuesta.value = "id: " + dato.id + "\nurl: " + dato.url + "\ncreado el: " + dato.created_at;
+            });
     } else if (iTipo == '' && iFiltro != '') {
         fetch(`https://cataas.com/cat?type=${iFiltro}&json=true`)
             .then(response => response.json())
-            .then(dato => { imagen.setAttribute('src', `https://cataas.com/${dato.url}`) })
+            .then(dato => {
+                imagen.setAttribute('src', `https://cataas.com/${dato.url}`);
+                respuesta.value = "id: " + dato.id + "\nurl: " + dato.url + "\ncreado el: " + dato.created_at;
+            });
     }
 }
